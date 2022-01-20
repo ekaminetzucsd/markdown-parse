168a10fb67319a640c499bde97187cab9586b9a3
ekaminetzucsd <ekaminetz@ucsd.edu>
2022-01-20 12:28:06 -0800

    fixed text between brac and parens

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -18,6 +18,11 @@ public class MarkdownParse {
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
+	    if((nextOpenBracket != 0 && markdown.charAt(nextOpenBracket - 1) == '!') ||
+	       nextCloseBracket + 1 != openParen) {
+		currentIndex = closeParen + 1;
+		continue;
+	    }
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
         }
